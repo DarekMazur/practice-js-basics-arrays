590a04718ca0bd9a78316532750b07f8977f236a
Darek Mazur <dev@darekmazur.it>
2024-05-28 13:51:27 +0200

    Implement solution

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenSum = numbers.filter(number => number % 2 === 0).reduce((fallback, currentValue) => fallback + currentValue);
+
+console.log(evenSum)
